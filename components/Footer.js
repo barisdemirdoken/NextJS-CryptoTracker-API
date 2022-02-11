8014e0e747b794ab02d9b11eb83771aac3e42ab5
barisdemirdoken <barisdemirdoken@icloud.com>
2022-02-11 05:10:41 +0300

    ilk commit

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from "react";
+import Style from "../components/SearchBar/Search.module.css"
+
+class Footer extends Component {
+    render() {
+        return (
+            <div className="footercss">
+                <div>
+                    
+                    © Created By Barış Demirdöken..
+        </div>
+            </div>
+        )
+    }
+}
+
+export default Footer;
